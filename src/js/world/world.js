1629cbac4bd611c3b479f995656aa511f8f1bf99
hexianWeb <hexianWeb@gmail.com>
2025-02-20 14:48:23 +0800

    feat: 调整 场景基础设置

--- src/js/world/world.js
+++ src/js/world/world.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
-import Float from '../components/float.js';
 import Experience from '../experience.js';
 import Environment from './environment.js';
+import NormalizedBall from './normalized-ball.js';
 
 export default class World {
   constructor() {
@@ -10,26 +10,19 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    this.float = new Float({ speed: 1.5, floatIntensity: 2 });
-
+    this.scene.add(new THREE.AxesHelper(5));
     // Environment
     this.resources.on('ready', () => {
       // Setup
       this.environment = new Environment();
     });
-    // Test mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial({ color: 0xFF_00_00 })
-    );
-    this.float.add(testMesh);
-    // this.scene.add(testMesh);
+    // 归一化小球
+    this.normalizedBall = new NormalizedBall();
   }
 
   update() {
-    if (this.float) {
-      console.log('update float');
-      this.float.update();
+    if (this.normalizedBall) {
+      this.normalizedBall.update();
     }
   }
 }
